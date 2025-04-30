18125727e41bc669f7a2d6787d269f02c6d93a07
Yura Chornyi <chorny.yura@gmail.com>
2025-05-01 02:29:23 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -10,7 +10,7 @@ from selenium.common import (
     ElementClickInterceptedException,
 )
 from selenium.webdriver.common.by import By
-from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as ex_con
 from selenium.webdriver.support.wait import WebDriverWait
